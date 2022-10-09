27b6284489100d5acf7376a9ac38c31d7385d743
Yayunyun <110958060+Yayunyun@users.noreply.github.com>
2022-10-09 15:29:31 +0000

    add dockerfile  and push to docker hub

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
 FROM alpine:latest
 RUN apk update && apk add bash
+
+WORKDIR /app
+COPY info.sh /app
--- project_alias.sh
+++ project_alias.sh
@@ -2,5 +2,7 @@
 alias download='kaggle datasets download mokar2001/ascvd-heart-risk -f heartRisk.csv && mv heartRisk.csv full_data.csv'
 # see columns of the file
 alias info='./info.sh'
+# truncate the file
 alias truncate='./truncate.sh'
+# categorize the file
 alias categorize='./categorize.sh'
